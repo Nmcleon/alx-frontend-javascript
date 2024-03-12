79e1f9c72b27587f4c658ae30a8e72b17e59c2f7
Nmcleon <nmcleon10@gmail.com>
2024-03-12 11:06:53 +0000

    add main files

--- 0x03-ES6_data_manipulation/2-main.js
+++ 0x03-ES6_data_manipulation/2-main.js
@@ -4,10 +4,3 @@ import getStudentsByLocation from "./2-get_students_by_loc.js";
 const students = getListStudents();
 
 console.log(getStudentsByLocation(students, 'San Francisco'));
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 2-main.js 
-[
-  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
-]
